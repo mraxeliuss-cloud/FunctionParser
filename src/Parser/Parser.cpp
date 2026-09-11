e92273616c63b8202a472028f4f79eceedff173a
MrAxel <mraxeliuss@gmail.com>
2026-09-11 16:31:33 +0200

    Separar implementación y header En el commit anterior había un error, la implementación del algoritmo estaba en el .h TBD: ""

--- src/Parser/Parser.cpp
+++ src/Parser/Parser.cpp
@@ -1,3 +1,55 @@
 #include "Parser.h"
 
-void Parser::parsear()
+#include <string>
+#include <vector>
+#include <memory>
+#include <variant>
+#include <stack>
+
+#include "Token.h"
+#include "Nodo.h"
+
+void Parser::ShuntingYard(std::vector<Token> &tokens)
+{
+    std::stack<Token> pilaOperadores;
+    std::stack<std::unique_ptr<Nodo>> pilaOperandos;
+    for (const auto &elemento : tokens)
+    {
+        // Esto son los operadores
+        if (elemento.tipo >= TokenType::ADD && elemento.tipo <= TokenType::DIVIDE)
+        {
+            while (!pilaOperadores.empty() && prioridad(pilaOperadores.top()) >= prioridad(elemento))
+            {
+                auto sacado = pilaOperadores.top();
+                pilaOperadores.pop();
+                auto hijoD = std::move(pilaOperandos.top());
+                pilaOperandos.pop();
+                auto hijoI = std::move(pilaOperandos.top());
+                pilaOperandos.pop();
+                auto nodo = std::make_unique<Nodo>(sacado, std::move(hijoI), std::move(hijoD));
+                pilaOperandos.push(std::move(nodo));
+            }
+            pilaOperadores.push(elemento);
+        }
+        else // De momento asumimos que solo son operadores báscos o números
+        {
+            auto nodo = std::make_unique<Nodo>(elemento, nullptr, nullptr);
+            pilaOperandos.push(std::move(nodo));
+        }
+    }
+}
+
+int Parser::prioridad(const Token &token) const
+{
+    switch (token.tipo)
+    {
+    case TokenType::ADD: // +
+        return 1;
+    case TokenType::SUB: // -
+        return 1;
+    case TokenType::MULT: // *
+        return 2;
+    case TokenType::DIVIDE: // /
+        return 2;
+    }
+}
--- src/Parser/Parser.h
+++ src/Parser/Parser.h
@@ -17,55 +17,8 @@ public:
     ~Parser() = default;
 
     std::vector<std::variant<int, float, char, std::string>> parsear(const std::vector<Token> &);
-    /*
-            Shunting yard
-            Construir el AST durante el propio parseing
-            2 vectores pila.
 
-        ESQUEMA DEL ALGORITMO
-    */
-    void ShuntingYard(std::vector<Token> &tokens)
-    {
-        std::stack<Token> pilaOperadores;
-        std::stack<std::unique_ptr<Nodo>> pilaOperandos;
-        for (const auto &elemento : tokens)
-        {
-            // Esto son los operadores
-            if (elemento.tipo >= TokenType::ADD && elemento.tipo <= TokenType::DIVIDE)
-            {
-                while (!pilaOperadores.empty() && prioridad(pilaOperadores.top()) >= prioridad(elemento))
-                    {
-                        auto sacado = pilaOperadores.top();
-                        pilaOperadores.pop();
-                        auto hijoD = std::move(pilaOperandos.top());
-                        pilaOperandos.pop();
-                        auto hijoI = std::move(pilaOperandos.top());
-                        pilaOperandos.pop();
-                        auto nodo = std::make_unique<Nodo>(sacado, std::move(hijoI), std::move(hijoD));
-                        pilaOperandos.push(std::move(nodo));
-                    }
-                pilaOperadores.push(elemento);
-            }
-            else // De momento asumimos que solo son operadores báscos o números
-            {
-                auto nodo = std::make_unique<Nodo>(elemento, nullptr, nullptr);
-                pilaOperandos.push(std::move(nodo));
-            }
-        }
-    }
+    void ShuntingYard(std::vector<Token> &tokens);
 
-    int prioridad(const Token &token) const
-    {
-        switch (token.tipo)
-        {
-        case TokenType::ADD : // +
-            return 1;
-        case TokenType::SUB : // -
-            return 1;
-        case TokenType::MULT : // *
-            return 2;
-        case TokenType::DIVIDE : // /
-            return 2;
-        }
-    }
+    int prioridad(const Token &token) const;
 };
